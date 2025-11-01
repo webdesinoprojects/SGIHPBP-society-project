7828a68012e00d07ceff612310a08af5214e8266
Manya <manya111203@gmail.com>
2025-11-01 20:12:58 +0530

    update: server.js

--- backend/server.js
+++ backend/server.js
@@ -15,27 +15,10 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 4000
-const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || 'https://sgihpbp-society-project.vercel.app/' || 'http://localhost:5173'
-
-const allowedOrigins = [
-  process.env.FRONTEND_ORIGIN, // Your Vercel URL (handles with/without slash)
-  'http://localhost:5173'       // Your local dev URL
-];
-
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || 'https://sgihpbp-society-project.vercel.app/' ||'http://localhost:5173'
 
 app.use(express.json())
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like Postman/curl) or from our list
-    // We use .some() and .startsWith() to avoid issues with trailing slashes
-    if (!origin || allowedOrigins.some(allowed => origin.startsWith(allowed))) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors({ origin: FRONTEND_ORIGIN }))
 app.use('/public', express.static(path.join(process.cwd(), 'backend', 'public')))
 
 // init DB
